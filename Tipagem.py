1a8c3f26c4dac86b9c3f5bd461875fe03cba56c1
Thiago Fabri <thiagobarrosfabri07@gmail.com>
2025-07-22 11:26:37 -0300

    fist commit

---
+++ Tipagem.py
@@ -0,0 +1,22 @@
+"""
+DocString
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+print(1234)
+
+# Aspas simples
+print('Thiago Fabri')
+print(1, 'Thiago "Fabri"')
+
+# Aspas duplas
+print("Thiago Fabri")
+print(2, "Thiago'Fabri'")
+
+# Escape
+print("Thiago \"Fabri\"")
+
+# r
+print(r"Thiago \"Fabri\"")
