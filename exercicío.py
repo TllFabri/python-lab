9c9f97aa7b06fb612ec4093b637a992288ed32de
Thiago Fabri <thiagobarrosfabri07@gmail.com>
2025-08-13 14:09:51 -0300

    Thiago tem 1.77 de altura, pesa 70 quilos e seu imc é 22.34351559258195

---
+++ exercicío.py
@@ -0,0 +1,14 @@
+nome = 'Thiago'
+altura = 1.77
+peso = 70
+imc = peso / altura ** 2
+
+print(nome, 'tem', altura, 'de altura,',)
+print('pesa', peso, 'quilos e seu imc é',)
+print(imc)
+
+"""
+Thiago tem 1.77 de altura,
+pesa 70 quilos e seu imc é
+22.34351559258195
+"""
