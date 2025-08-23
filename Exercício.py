b20d8448d844efad04d3f30494f48a3ac5ce0ffe
Thiago Fabri <thiagobarrosfabri07@gmail.com>
2025-08-23 14:48:30 -0300

    Solução - Exercício de programação com if e comparação

---
+++ Exercício.py
@@ -0,0 +1,8 @@
+primeiro_valor = int(input('Digite o primeiro valor: '))
+segundo_valor = int(input('Digite o segundo valor: '))
+
+if primeiro_valor >= segundo_valor:
+    print(f'O {primeiro_valor} é maior ou igual que {segundo_valor}')
+
+else:
+    print(f'O {primeiro_valor} é menor que {segundo_valor}')
